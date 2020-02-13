7a3b70b561d02f7411c4f71be78a60fa02073ef5
cheroliv <cheroliv@laposte.net>
2020-02-13 15:06:57 +0100

    ___

--- routes/promos.routes.js
+++ routes/promos.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const _ = require("lodash");
 const router = express.Router();
 const promosController = require("../controllers/promos.controller");
 
@@ -15,19 +16,60 @@ router.get("/:id", async (request, response) => {
 
 router.post("/ajouter", async (request, response) => {
   const { titre, iteration } = request.body;
-  const nouvellePromo = await promosController.addPromo({ titre, iteration });
+  //validation des données
+if(_.isNil(titre)){
+  response.status(400)
+    .json({error:'le titre doit etre renseigné'});
+}
 
-  response.status(201).json({
+if(typeof titre !== 'string'){
+  response.status(400)
+    .json({error:'le titre doit etre une chaine de caracteres'});
+}
+
+if(titre.length>30){
+  response.status(400)
+    .json({error:'le titre doit etre inferieure à 30 caracteres'});
+}
+
+ if(_.isNil(iteration)){
+  response.status(400)
+  .json({error:'l\'itération doit etre renseigné'});
+ }
+
+ if(typeof iteration !== 'number'){
+  response.status(400)
+    .json({error:'l\'itération doit etre un nombre'});
+}
+
+
+if(!Number.isInteger(iteration)){
+  response.status(400)
+    .json({error:'l\'itération doit etre un nombre entier'});
+}
+
+if(iteration<=0){
+  response.status(400)
+    .json({error:'l\'itération doit etre un nombre positif'});
+}
+
+const nouvellePromo = await promosController
+        .addPromo({ titre, iteration });
+
+response.status(201).json({
     data: {
       titre: nouvellePromo.titre,
       iteration: nouvellePromo.iteration
     }
   });
 });
+
+
 router.delete("/supprimer/:id", async(request,response)=>{
   const { id } = request.params;
   await promosController.deletePromo(id);
-  response.status(204).json();
+  response.json()
+      .status(204);
 
 });
 router.put("/modifier/:id", async(request, response)=>{
